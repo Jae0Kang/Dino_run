e97de59068043ad821ad5079403e10c29236c7a0
강재영 <jaeyoungkang@wirelessprv-10-194-35-99.near.illinois.edu>
2022-09-28 21:30:58 -0500

    ect

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ pygame.init()
 #screen_size_display = (width_screen, height_screen) = (600, 150)
 width_screen = 600
 height_screen = 150
+FPS = 60
+GRAVITY = 0.8
 
-display_surface = pygame.display.set_mode((width_screen, height_screen))    #display
-pygame.display.set_caption("Dino run")                                      #window 
 
-WHITE = (255,255,250)
+display_surface = pygame.display.set_mode((width_screen, height_screen))    #display 크기
+time_clok = pygame.time.Clock()                                             #시간 측정
+pygame.display.set_caption("Dino run")                                      #window 이름
+
+WHITE = (255,255,255)
 BLACK = (0,0,0)
 
 
