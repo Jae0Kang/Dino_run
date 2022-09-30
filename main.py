6f0ad797f6c2c893288c02f02fec2dd083a54577
강재영 <jaeyoungkang@wirelessprv-10-194-35-99.near.illinois.edu>
2022-09-29 21:41:02 -0500

    Santa moves

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 #from operator import truediv
 import pygame
+import os
+import random
 
 pygame.init()
-#screen_size_display = (width_screen, height_screen) = (600, 150)
-width_screen = 600
-height_screen = 150
-FPS = 60
-GRAVITY = 0.8
+width_screen = 900
+height_screen = 600
 
 
 display_surface = pygame.display.set_mode((width_screen, height_screen))    #display 크기
 time_clok = pygame.time.Clock()                                             #시간 측정
-pygame.display.set_caption("Dino run")                                      #window 이름
+pygame.display.set_caption("Santa run")                                      #window 이름
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
 
-display_surface.fill(WHITE)
+player = pygame.image.load("Player.png")
+player = pygame.transform.scale(player, (200, 150))
+player_Rect = player.get_rect()
+
+player_Rect.centerx = round(0)
+player_Rect.centery = round(500)
+
+
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.update()
+    if player_Rect.x > width_screen:
+        player_Rect.x = 0 - player_Rect.width
+    display_surface.fill(WHITE)
+    player_Rect.x += 100
+    display_surface.blit(player, player_Rect)        
+    pygame.display.flip()
+    time_clok.tick(3)
 
 pygame.quit()
+
+
